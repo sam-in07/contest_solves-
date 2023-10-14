e4214306c5ee1998410db7dd4720c358ad7e5fc5
sam-in07 <85004796+sam-in07@users.noreply.github.com>
2023-10-14 08:48:02 +0600

    Create icpcpiliE.cpp

---
+++ icpcpiliE.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+
+int main() {
+//attempt : samin 
+/*solver : samin  */
+/*team name : IIUC_Newbies*/
+   ll test;cin>>test;
+   for(ll t=1;t<=test;t++){
+  ll n;cin>>n;
+  ll ar[n];
+  for(ll i = 0;i<n;i++)cin>>ar[i];
+  ll ans = (n*(n+1))/2;
+  ans-=n;
+  ll c = 1;
+  for(ll i = 1;i<n;i++)
+  {
+   if(ar[i]==ar[i-1])c++;
+   else 
+   {
+      ll m = (c*(c+1))/2;
+
+      ans-=m;
+      c = 1;
+   }
+  }
+  ll m = (c*(c+1))/2;
+      m-=c;
+     ans-=m;
+      c = 1;
+  cout<<"Case "<<t<<": ";
+cout<<ans<<endl;
+
+   }   
+
+}
+    
+
+
+
+
